1559ef3d1814740b39e360c1124f08cadbfc8861
Nobody4931 <ardenxie1@gmail.com>
2020-07-24 18:47:11 -0400

    404 page not found

--- main.js
+++ main.js
@@ -14,7 +14,13 @@ const DBManager = require("./resources/modules/database.js");
 const Options = require("./resources/modules/options.json");
 
 /* BACKEND */
-App.get("/video.uwu", async (Request, Response) => Response.sendFile(`${__dirname}/resources/files/music.mp4`));
+App.get("/video.uwu", async (Request, Response) => (Request.headers["referer"] != null) ?
+	(Request.headers["referer"].match(/4931\.myftp\.org.*\/404/) != null) ? 
+		Response.sendFile(`${__dirname}/resources/files/music-2.mp4`) :
+		(Request.headers["referer"].match(/4931\.myftp\.org/) != null) ?
+			Response.sendFile(`${__dirname}/resources/files/music-1.mp4`) :
+			Response.status(400).send("no u") :
+		Response.status(400).send("no u"));
 App.get("/icon.uwu", async (Request, Response) => Response.sendFile(`${__dirname}/resources/files/favicon.png`));
 
 /* FRONTEND */
